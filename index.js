aaa7ebf1f54825eee9437fc73cf804c03cf4e6b7
Bivash Yadav <byadav02717@gmail.com>
2021-11-27 11:30:54 -0600

    removed user route

--- index.js
+++ index.js
@@ -2,22 +2,14 @@ const express = require('express')
 const cors = require('cors')
 
 
-const userRoutes = require("./routes/user")
 const itemRoutes = require("./routes/item")
 const middleware = require('./middleware/middleware')
-
-//let defaultAuth = getAuth(defaultApp)
-//let defaultDatabase = getDatabase(defaultApp)
 
 const app = express();
 app.use(express.json());
 app.use(cors({origin: true}));
 app.use(middleware.decodeToken)
 
-
-
-
-app.use("/user", userRoutes);
 app.use("/item", itemRoutes);
 
 

a0b2b0ee92375a66cbd219bc0340ff174bb24783
Bivash Yadav <byadav02717@gmail.com>
2021-11-27 11:26:48 -0600

    moved api to different routes

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const cors = require('cors')
-const db = require("./database")
+
+
+const userRoutes = require("./routes/user")
+const itemRoutes = require("./routes/item")
+const middleware = require('./middleware/middleware')
+
+//let defaultAuth = getAuth(defaultApp)
+//let defaultDatabase = getDatabase(defaultApp)
 
 const app = express();
 app.use(express.json());
 app.use(cors({origin: true}));
+app.use(middleware.decodeToken)
 
-app.post('/signup', (req, res) => {
-    (async () => {
-        try {
-          await db.collection('users').doc('/' + req.body.Email + '/')
-              .create(req.body);
-          return res.status(200).send();
-        } catch (error) {
-          console.log(error);
-          return res.status(500).send(error);
-        }
-      })();
-  });
+
+
+
+app.use("/user", userRoutes);
+app.use("/item", itemRoutes);
+
+
 
 app.listen(5000, ()=>console.log("Server running at 5000"))

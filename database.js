1da52bbb22c4657b2c5ec2b1c2bdbd6588ef1bfc
Bivash Yadav <byadav02717@gmail.com>
2021-11-27 11:28:05 -0600

    firestore db setup

--- database.js
+++ database.js
@@ -1,9 +1,10 @@
-const admin = require('firebase-admin');
-var serviceAccount = require("./permission.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://todolister-b1fb8-default-rtdb.firebaseio.com"
-});
+// const admin = require('firebase-admin');
+// var serviceAccount = require("./permission.json");
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://todolister-b1fb8-default-rtdb.firebaseio.com"
+// });
+const admin = require('./admin')
 const db = admin.firestore();
 
 module.exports = db
